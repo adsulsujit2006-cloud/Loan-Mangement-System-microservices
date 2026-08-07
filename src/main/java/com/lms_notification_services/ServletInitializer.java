4dfac21ae9ec32405470c3c206902a68811b7418
adsulsujit2006-cloud <adsulsujit2006@gmail.com>
2026-08-07 18:41:59 +0530

    complete notification services folder services

---
+++ src/main/java/com/lms_notification_services/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lms_notification_services;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmsNotificationServicesApplication.class);
+	}
+
+}
---
+++ src/test/java/com/lms_notification_services/LmsNotificationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms_notification_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsNotificationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
