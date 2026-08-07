4e665e16f765b23fcb09b66eec60f6a0a981eec4
adsulsujit2006-cloud <adsulsujit2006@gmail.com>
2026-08-07 18:49:15 +0530

    payment services complete folder strcture

---
+++ src/main/java/com/lms_payment_services/LmsPaymentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lms_payment_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsPaymentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsPaymentServicesApplication.class, args);
+	}
+
+}
