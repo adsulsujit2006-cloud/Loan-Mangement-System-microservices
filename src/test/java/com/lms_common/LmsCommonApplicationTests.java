4986eb1176cfe5bf6a06402ef73a8d10849424e7
adsulsujit2006-cloud <adsulsujit2006@gmail.com>
2026-08-07 19:41:42 +0530

    complete common folder strcture

---
+++ src/test/java/com/lms_common/LmsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
