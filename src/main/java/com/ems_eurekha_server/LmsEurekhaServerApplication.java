bf54b8f880c92c89b332255699b2a07f5fd4e58f
adsulsujit2006-cloud <adsulsujit2006@gmail.com>
2026-08-07 17:51:06 +0530

    Complete Eurekha Server

---
+++ src/main/java/com/ems_eurekha_server/LmsEurekhaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ems_eurekha_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LmsEurekhaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsEurekhaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ems_eurekha_server/LmsEurekhaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems_eurekha_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsEurekhaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
