4bb5bdbed73c7905c1ba6eea247cee5ba07471cd
adsulsujit2006-cloud <adsulsujit2006@gmail.com>
2026-08-09 10:54:23 +0530

    Add complete entity classes and related enums to user service

---
+++ src/main/java/com/lms_user_service/domain/Gender.java
@@ -0,0 +1,6 @@
+package com.lms_user_service.domain;
+
+public enum Gender {
+	Male, FeMale, Other
+
+}
---
+++ src/main/java/com/lms_user_service/modal/Address.java
@@ -0,0 +1,73 @@
+package com.lms_user_service.modal;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Long id;
+
+    @Column(name = "address_line_1", nullable = false, length = 255)
+    private String addressLine1;
+
+    @Column(name = "address_line_2", length = 255)
+    private String addressLine2;
+
+    @Column(name = "landmark", length = 150)
+    private String landmark;
+
+    @Column(name = "city", nullable = false, length = 100)
+    private String city;
+
+    @Column(name = "district", length = 100)
+    private String district;
+
+    @Column(name = "state", nullable = false, length = 100)
+    private String state;
+
+    @Column(name = "country", nullable = false, length = 100)
+    private String country;
+
+    @Column(name = "pin_code", nullable = false, length = 10)
+    private String pinCode;
+
+    @Column(name = "is_active")
+    private Boolean active = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToOne(mappedBy = "address")
+    private User user;
+
+
+}
