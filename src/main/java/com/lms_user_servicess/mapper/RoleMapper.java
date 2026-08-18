b90fabe434db5d2d140e5c6c37d9cb50a22107dd
Sujitclear <adsulsujit2006@gmail.com>
2026-08-18 22:39:46 +0530

    create role Rest api and implemted class and inteface

--- src/main/java/com/lms_user_servicess/mapper/RoleMapper.java
+++ src/main/java/com/lms_user_servicess/mapper/RoleMapper.java
@@ -2,11 +2,13 @@ package com.lms_user_servicess.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.lms_user_servicess.dto.request.CreateRoleRequest;
 import com.lms_user_servicess.dto.responce.RoleResponse;
 import com.lms_user_servicess.modal.Role;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
 	RoleResponse toResponse(Role role);
+	Role toEntity(CreateRoleRequest request);
 
 }
---
+++ src/main/java/com/lms_user_servicess/sservice/RoleServices.java
@@ -0,0 +1,25 @@
+package com.lms_user_servicess.sservice;
+
+import java.util.List;
+
+import com.lms_user_servicess.dto.request.CreateRoleRequest;
+import com.lms_user_servicess.dto.request.UpdateRoleRequest;
+import com.lms_user_servicess.dto.responce.ApiResponse;
+import com.lms_user_servicess.dto.responce.RoleResponse;
+
+public interface RoleServices {
+	public RoleResponse createRole(CreateRoleRequest request);
+
+	public RoleResponse updateRole(Long id, UpdateRoleRequest request);
+
+	public RoleResponse getRoleById(Long id);
+
+	public List<RoleResponse> getAllRole();
+
+	public ApiResponse deleteRole(Long id);
+
+	public ApiResponse ActiveRole(Long id);
+
+	public ApiResponse deActivateRole(Long id);
+
+}
